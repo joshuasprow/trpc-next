03e3413598aeae9068fb78e31e8917619c4eaae5
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-11-19 12:47:27 -0500

    adding next docs link back to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,11 @@ export default function Home() {
             <p>Browse employee records fetched from the TRPC backend.</p>
           </Link>
 
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
